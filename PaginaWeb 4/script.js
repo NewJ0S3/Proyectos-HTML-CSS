bb6b75ee6ac8f1e55bc3ce3d6fffcae955ba7060
NewJ0S3 <alexme30@gmail.com>
2023-10-02 10:49:24 -0500

    Proyectos de practica HTML y CSS

---
+++ PaginaWeb 4/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 80,
+    grabCursor: true,
+    loop: true,
+    breackpoints: {
+        991: {
+            slidesPerView: 3
+        }
+    }
+})
